dcba2822d6242732353ef74551744454ac786f58
Isa Turku <turkuisa00@gmail.com>
2024-07-10 19:52:29 +0200

    fix: duplicate key

--- src/state/product-finder-store.ts
+++ src/state/product-finder-store.ts
@@ -2,7 +2,6 @@ import { createStore } from "solid-js/store";
 
 type StoreFilterT = {
   productCategory: "new" | "rental";
-  productCategory: "new" | "rental";
 };
 
 export default createStore<StoreFilterT>({
