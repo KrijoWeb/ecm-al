62c2c2dfd318257b7b577822d37e1a90c1e6dc9d
Isa Turku <turkuisa00@gmail.com>
2025-03-02 19:44:19 +0100

    fix: nav

--- src/components/homepage/ServiceCard.tsx
+++ src/components/homepage/ServiceCard.tsx
@@ -5,6 +5,7 @@ interface Props {
   img?: string;
   active?: boolean;
   title: string;
+  text: string;
 }
 
 const ServiceCard: Component<Props> = (props) => {
@@ -22,19 +23,7 @@ const ServiceCard: Component<Props> = (props) => {
       >
         {props.title}
       </h2>
-      <p class="line-clamp-3 grow-0 text-justify">
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
-        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
-        Lorem duis laboris cupidatat officia voluptate. Culpa proident
-        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
-        et culpa duis.
-      </p>
+      <p class="line-clamp-3 grow-0 text-justify">{props.text}</p>
       <div class="self-center">
         <ArrowButton link="services" type="right" text="Lexo me shume" />
       </div>
