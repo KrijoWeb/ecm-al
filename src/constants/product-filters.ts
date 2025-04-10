5a3cd7341d501ae8c8bd2722870312888d77f875
Isa Turku <turkuisa00@gmail.com>
2025-04-10 10:42:04 +0200

    fix: FAQ

--- src/constants/product-filters.ts
+++ src/constants/product-filters.ts
@@ -1,8 +1,42 @@
-export default [
-  "Applications",
-  "Drive Types",
-  "Product Type",
-  "Load Capacity",
-  "Lift Height",
-  "Explosion Protection",
-];
+export default {
+  Applications: {
+    type: "select",
+    options: [
+      "Double Deck",
+      "Narrow Aisle",
+      "Loading/Unloading Dock",
+      "Loading/Unloading over the side",
+      "Order Picking",
+      "Moving",
+      "Stacking",
+      "Towing",
+    ],
+  },
+  "Drive Types": {
+    type: "select",
+    options: ["Manual", "Elektrik", "Nafte/Gaz"],
+  },
+  "Product Type": {
+    type: "select",
+    options: [
+      "Transpalete",
+      "Terheqes ngarkesash",
+      "Stivues per hapsira te ngushta",
+      "Transpalet stivuese",
+      "Komisionues Transpalet",
+      "Pirun elektrik",
+      "Pirun me nafte/gaz",
+      "Tren terheqes per magazine",
+      "Mjete automatike",
+    ],
+  },
+  "Load Capacity": {
+    type: "numeric",
+  },
+  "Lift Height": {
+    type: "numeric",
+  },
+  "Explosion Protection": {
+    type: "boolean",
+  },
+} as const;
